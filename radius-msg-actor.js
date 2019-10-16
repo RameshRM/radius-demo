78e8bdfd99dcdd5f3add18d60e0b7ca163ebe08f
Mahadevan <ramahadevan@lm-sjc-11016381.corp.ebay.com>
2019-10-16 13:48:29 -0700

    Updated process env

--- radius-msg-actor.js
+++ radius-msg-actor.js
@@ -5,7 +5,9 @@ const SuperAgent = require('superagent');
 module.exports.act = act;
 
 function act(options, next) {
-  SuperAgent.post('http://localhost:3333').query(options).timeout(100).retry(2)
+  let urlPath = process.env.URL_PATH || 'http://localhost:3333';
+
+  SuperAgent.post(urlPath).query(options).timeout(100).retry(2)
     .end(function(err, resp) {
       return next(undefined, {
         status: resp && resp.statusCode
